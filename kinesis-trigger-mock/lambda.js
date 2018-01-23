74f5da709201c48627c634bf8926b7ffba0c92b4
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-24 02:09:03 +0530

    Added DynamoDB put Item operation

--- kinesis-trigger-mock/lambda.js
+++ kinesis-trigger-mock/lambda.js
@@ -1,6 +1,16 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 
+	ddb.put({
+		TableName: 'chamath-test-dynamo',
+		Item: { 'ID': '001', 'Name': 'Chamath', 'Message': 'Hello from Sigma' }
+	}, function (err, data) {
+		if (err) {
+			callback(err, 'Error in executing DynamoDB#putItem');
+		} else {
+			callback(null, data);
+		}
+	});
 
-	callback(null,'Successfully executed');
 }
